b2541dbfecc008eb318a66a06619cc5eed09cf61
ytrav <withertrav394@gmail.com>
2022-05-23 13:03:08 +0200

    wip

---
+++ script.js
@@ -0,0 +1,8 @@
+var q = document.getElementsByClassName("q")
+
+function start() {
+    document.getElementById("field").style.display = "block";
+    document.getElementById("butt").style.display = "none";
+    document.getElementById
+}
+
